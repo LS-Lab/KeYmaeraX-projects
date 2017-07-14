c0c8cb5a0720354b4e450b3d583e132c49a497ab
Andre Platzer <aplatzer@cs.cmu.edu>
2017-07-14 16:16:23 +0200

    Adapt dG syntax

--- lics/dlds.sh
+++ lics/dlds.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-cat lics1-continuous-forward.kya lics2-hybrid-forward.kya  lics4a-time-safe.kya bouncing-ball.kya rotational.kya damposc.kya exp.kya > dlds.kya 
+cat lics1-continuous-forward.kya lics2-hybrid-forward.kya  lics4a-time-safe.kya bouncing-ball.kya rotational.kya damposc.kya exp.kya ../dlds/parachute.kya > dlds.kya 
 perl -pe 's/\xEF\xBB\xBF//g' < dlds.kya > ../dlds/dlds.kya
 ## sed 's/^\xfe\xff//' < dlds.kya > ../dlds/dlds.kya
 ## sed 's/^\xef\xbb\xbf//' < dlds.kya > ../dlds/dlds.kya
