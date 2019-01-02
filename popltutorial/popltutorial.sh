a192b1e9b373df98c76464fb839951fc70265bea
Andre Platzer <aplatzer@cs.cmu.edu>
2019-01-02 15:48:07 +0100

    Exercise

--- popltutorial/popltutorial.sh
+++ popltutorial/popltutorial.sh
@@ -3,7 +3,8 @@
 sed "s/ArchiveEntry *\".*\"/ArchiveEntry \"POPL: 1 Bouncing ball\"/" < ../lics/bouncing-ball.kya >> tmp.kya
 sed "s/ArchiveEntry *\".*\"/ArchiveEntry \"POPL: 2 Car drives forward\"/" < ../lics/lics2-hybrid-forward.kya >> tmp.kya 
 sed "s/ArchiveEntry *\".*\"/ArchiveEntry \"POPL: 3 Safely stopping car\"/" < ../lics/lics4a-time-safe.kya >> tmp.kya
-sed "s/ArchiveEntry *\".*\"/ArchiveEntry \"POPL: 4 CurveBot\"/" < ../lics/CurveBotDef.kyx >> tmp.kya
+sed "s/ArchiveEntry *\".*\"/ArchiveEntry \"POPL: 4 CurveBot Exercise\"/" < ../lics/CurveBotExc.kyx >> tmp.kya
+sed "s/ArchiveEntry *\".*\"/ArchiveEntry \"POPL: 4 CurveBot Solution\"/" < ../lics/CurveBotDef.kyx >> tmp.kya
 sed "s/ArchiveEntry *\".*\"/ArchiveEntry \"POPL: 5 Rotational dynamics\"/" < ../lics/rotational.kya >> tmp.kya
 cat > repls <<- EOM
 s/ArchiveEntry *".*"/ArchiveEntry "POPL: 6 Damped oscillator"/1
