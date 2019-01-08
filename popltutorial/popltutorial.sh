4b46d08813c67f65f1752f1a6a6c4102e462657a
Andre Platzer <aplatzer@cs.cmu.edu>
2019-01-08 14:53:51 +0100

    popl assembly

--- popltutorial/popltutorial.sh
+++ popltutorial/popltutorial.sh
@@ -1,18 +1,17 @@
 #!/bin/sh
 [[ -e tmp.kya ]] && rm tmp.kya
 sed "s/ArchiveEntry *\".*\"/ArchiveEntry \"POPL: 1 Bouncing ball\"/" < ../lics/bouncing-ball-if.kyx >> tmp.kya
-sed "s/ArchiveEntry *\".*\"/ArchiveEntry \"POPL: 2 Car drives forward\"/" < ../lics/lics2-hybrid-forward.kya >> tmp.kya 
-sed "s/ArchiveEntry *\".*\"/ArchiveEntry \"POPL: 3 Safely stopping car\"/" < ../lics/lics4a-time-safe.kya >> tmp.kya
-sed "s/Exercise *\".*\"/Exercise \"POPL: 4 CurveBot Exercise\"/" < ../lics/CurveBotExc.kyx >> tmp.kya
-sed "s/ArchiveEntry *\".*\"/ArchiveEntry \"POPL: 4 CurveBot Solution\"/" < ../lics/CurveBotDef.kyx >> tmp.kya
-sed "s/ArchiveEntry *\".*\"/ArchiveEntry \"POPL: 5 Rotational dynamics\"/" < ../lics/rotational.kya >> tmp.kya
+#sed "s/ArchiveEntry *\".*\"/ArchiveEntry \"POPL: 2 Car drives forward\"/" < ../lics/lics2-hybrid-forward.kya >> tmp.kya 
+sed "s/ArchiveEntry *\".*\"/ArchiveEntry \"POPL: 2 Safely stopping car\"/" < ../lics/lics4a-time-safe.kya >> tmp.kya
+sed "s/Exercise *\".*\"/Exercise \"POPL: 3 CurveBot Exercise\"/" < ../lics/CurveBotExc.kyx >> tmp.kya
+sed "s/ArchiveEntry *\".*\"/ArchiveEntry \"POPL: 3 CurveBot Solution\"/" < ../lics/CurveBotDef.kyx >> tmp.kya
 cat > repls <<- EOM
-s/ArchiveEntry *".*"/ArchiveEntry "POPL: 6 Damped oscillator"/1
-s/ArchiveEntry *".*"/ArchiveEntry "POPL: 6 Increasingly damped oscillator"/2
+s/ArchiveEntry *".*"/ArchiveEntry "POPL: 4 Damped oscillator"/1
+s/ArchiveEntry *".*"/ArchiveEntry "POPL: 4 Increasingly damped oscillator"/2
 EOM
 sed -f repls < ../lics/damposc.kya >> tmp.kya
-sed "s/Exercise *\".*\"/Exercise \"POPL: 7 Parachute Exercise\"/" < ../dlds/parachuteExc.kyx >> tmp.kya
-sed "s/ArchiveEntry *\".*\"/ArchiveEntry \"POPL: 7 Parachute Solution\"/" < ../dlds/parachute.kya >> tmp.kya
+sed "s/Exercise *\".*\"/Exercise \"POPL: 5 Parachute Exercise\"/" < ../dlds/parachuteExc.kyx >> tmp.kya
+sed "s/ArchiveEntry *\".*\"/ArchiveEntry \"POPL: 5 Parachute Solution\"/" < ../dlds/parachute.kya >> tmp.kya
 
 #cat ../lics/bouncing-ball.kya  ../lics/lics2-hybrid-forward.kya ../lics/lics4a-time-safe.kya ../lics/CurveBotDef.kya ../lics/rotational.kya ../lics/damposc.kya ../dlds/parachute.kya > tmp.kya 
 perl -pe 's/\xEF\xBB\xBF//g' < tmp.kya > popltutorial.kyx
