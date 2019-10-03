1bf1b5c3b6ba5b9b8403c9c3f99e28b6dc5c4500
Andre Platzer <aplatzer@cs.cmu.edu>
2019-10-03 08:40:32 -0400

    Improved compilation

--- dlds/make.sh
+++ dlds/make.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-cat ../lics/lics1-continuous-forward.kya ../lics/lics2-hybrid-forward.kya ../lics/lics4a-time-safe.kya ../lics/bouncing-ball.kya ../lics/rotational.kya ../lics/damposc.kya ../lics/exp.kya ../dlds/parachute.kya ../games/dual-filibuster.kya ../games/pusharound-cart.kya ../games/WALL-E-EVE.kya ../games/goalie.kya > dlds-building.kya 
+cat ../lics/lics1-continuous-forward.kya ../lics/lics2-hybrid-forward.kya ../lics/lics4a-time-safe.kya ../lics/bouncing-ball.kya ../lics/rotational.kya ../lics/damposc.kyx ../lics/exp.kya ../dlds/parachute.kyx ../games/dual-filibuster.kya ../games/pusharound-cart.kya ../games/WALL-E-EVE.kya ../games/goalie.kya > dlds-building.kya 
 perl -pe 's/\xEF\xBB\xBF//g' < dlds-building.kya > ../dlds/dlds.kya
 ## sed 's/^\xfe\xff//' < dlds.kya > ../dlds/dlds.kya
 ## sed 's/^\xef\xbb\xbf//' < dlds.kya > ../dlds/dlds.kya
