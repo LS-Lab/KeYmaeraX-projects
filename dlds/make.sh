d91603e9b618fc5ccda1064447151ba8777d1078
Stefan Mitsch <smitsch@cs.cmu.edu>
2020-09-19 10:43:32 -0400

    DLDS folder

--- dlds/make.sh
+++ dlds/make.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 [[ -e tmp.kya ]] && rm tmp.kya
-sed "s/ArchiveEntry *\"\(.*\)\"/ArchiveEntry \"01 Continuous car accelerates\"/" < ../lics/lics1-continuous-forward.kyx >> tmp.kya
-sed "s/ArchiveEntry *\"\(.*\)\"/ArchiveEntry \"02 Single car drives forward\"/" < ../lics/lics2-hybrid-forward.kyx >> tmp.kya
-sed "s/ArchiveEntry *\"\(.*\)\"/ArchiveEntry \"03 Safe stopping of time-triggered car\"/" < ../lics/lics4a-time-safe.kyx >> tmp.kya
-sed "s/ArchiveEntry *\"\(.*\)\"/ArchiveEntry \"04 \1\"/" < ../lics/bouncing-ball.kyx >> tmp.kya
-sed "s/ArchiveEntry *\"\(.*\)\"/ArchiveEntry \"05 \1\"/" < ../lics/rotational.kyx >> tmp.kya
-sed "s/ArchiveEntry *\"\(.*\)\"/ArchiveEntry \"06 \1\"/" < ../lics/damposc.kyx >> tmp.kya
-sed "s/ArchiveEntry *\"\(.*\)\"/ArchiveEntry \"07 \1\"/" < ../lics/exp-decay.kyx >> tmp.kya
-sed "s/ArchiveEntry *\"\(.*\)\"/ArchiveEntry \"08 \1\"/" < ../dlds/parachute.kyx >> tmp.kya
-sed "s/ArchiveEntry *\"\(.*\)\"/ArchiveEntry \"09 \1\"/" < ../games/dual-filibuster.kyx >> tmp.kya
-sed "s/ArchiveEntry *\"\(.*\)\"/ArchiveEntry \"10 \1\"/" < ../games/pusharound-cart.kyx >> tmp.kya
-sed "s/ArchiveEntry *\"\(.*\)\"/ArchiveEntry \"11 \1\"/" < ../games/WALL-E-EVE.kyx >> tmp.kya
-sed "s/ArchiveEntry *\"\(.*\)\"/ArchiveEntry \"12 \1\"/" < ../games/goalie.kyx >> tmp.kya
+sed "s/ArchiveEntry *\"\(.*\)\"/ArchiveEntry \"DLDS\/01 Continuous car accelerates\"/" < ../lics/lics1-continuous-forward.kyx >> tmp.kya
+sed "s/ArchiveEntry *\"\(.*\)\"/ArchiveEntry \"DLDS\/02 Single car drives forward\"/" < ../lics/lics2-hybrid-forward.kyx >> tmp.kya
+sed "s/ArchiveEntry *\"\(.*\)\"/ArchiveEntry \"DLDS\/03 Safe stopping of time-triggered car\"/" < ../lics/lics4a-time-safe.kyx >> tmp.kya
+sed "s/ArchiveEntry *\"\(.*\)\"/ArchiveEntry \"DLDS\/04 \1\"/" < ../lics/bouncing-ball.kyx >> tmp.kya
+sed "s/ArchiveEntry *\"\(.*\)\"/ArchiveEntry \"DLDS\/05 \1\"/" < ../lics/rotational.kyx >> tmp.kya
+sed "s/ArchiveEntry *\"\(.*\)\"/ArchiveEntry \"DLDS\/06 \1\"/" < ../lics/damposc.kyx >> tmp.kya
+sed "s/ArchiveEntry *\"\(.*\)\"/ArchiveEntry \"DLDS\/07 \1\"/" < ../lics/exp-decay.kyx >> tmp.kya
+sed "s/ArchiveEntry *\"\(.*\)\"/ArchiveEntry \"DLDS\/08 \1\"/" < ../dlds/parachute.kyx >> tmp.kya
+sed "s/ArchiveEntry *\"\(.*\)\"/ArchiveEntry \"DLDS\/09 \1\"/" < ../games/dual-filibuster.kyx >> tmp.kya
+sed "s/ArchiveEntry *\"\(.*\)\"/ArchiveEntry \"DLDS\/10 \1\"/" < ../games/pusharound-cart.kyx >> tmp.kya
+sed "s/ArchiveEntry *\"\(.*\)\"/ArchiveEntry \"DLDS\/11 \1\"/" < ../games/WALL-E-EVE.kyx >> tmp.kya
+sed "s/ArchiveEntry *\"\(.*\)\"/ArchiveEntry \"DLDS\/12 \1\"/" < ../games/goalie.kyx >> tmp.kya
 
 #cat ../lics/lics1-continuous-forward.kya ../lics/lics2-hybrid-forward.kya ../lics/lics4a-time-safe.kya ../lics/bouncing-ball.kya ../lics/rotational.kya ../lics/damposc.kyx ../lics/exp.kya ../dlds/parachute.kyx ../games/dual-filibuster.kyx ../games/pusharound-cart.kyx ../games/WALL-E-EVE.kyx ../games/goalie.kyx > dlds-building.kya 
 
