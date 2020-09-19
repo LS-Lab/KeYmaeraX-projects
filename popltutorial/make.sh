1b57fb3e81cf2243f7132a381433b88c5c964f80
Stefan Mitsch <smitsch@cs.cmu.edu>
2020-09-19 10:42:13 -0400

    Parachute ODE invariant annotation fix

--- popltutorial/make.sh
+++ popltutorial/make.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 [[ -e tmp.kya ]] && rm tmp.kya
-sed "s/ArchiveEntry *\".*\"/ArchiveEntry \"POPL: 1 Bouncing ball\"/" < ../lics/bouncing-ball-if.kyx >> tmp.kya
-#sed "s/ArchiveEntry *\".*\"/ArchiveEntry \"POPL: 2 Car drives forward\"/" < ../lics/lics2-hybrid-forward.kya >> tmp.kya 
-sed "s/ArchiveEntry *\".*\"/ArchiveEntry \"POPL: 2 Safely stopping car\"/" < time-safe.kyx >> tmp.kya
-sed "s/Exercise *\".*\"/Exercise \"POPL: 3 Exercise: CurveBot\"/" < ../lics/CurveBotExc.kyx >> tmp.kya
-sed "s/ArchiveEntry *\".*\"/ArchiveEntry \"POPL: 3 Solution: CurveBot\"/" < ../lics/CurveBotDef.kyx >> tmp.kya
-sed "s/ArchiveEntry *\".*\"/Exercise \"POPL: 4 Exercise: Bouncing ball\"/" < ../lics/bouncing-ball-exc.kyx >> tmp.kya
+sed "s/ArchiveEntry *\".*\"/ArchiveEntry \"POPL\/POPL: 1 Bouncing ball\"/" < ../lics/bouncing-ball-if.kyx >> tmp.kya
+#sed "s/ArchiveEntry *\".*\"/ArchiveEntry \"POPL\/POPL: 2 Car drives forward\"/" < ../lics/lics2-hybrid-forward.kya >> tmp.kya
+sed "s/ArchiveEntry *\".*\"/ArchiveEntry \"POPL\/POPL: 2 Safely stopping car\"/" < time-safe.kyx >> tmp.kya
+sed "s/Exercise *\".*\"/Exercise \"POPL\/POPL: 3 Exercise: CurveBot\"/" < ../lics/CurveBotExc.kyx >> tmp.kya
+sed "s/ArchiveEntry *\".*\"/ArchiveEntry \"POPL\/POPL: 3 Solution: CurveBot\"/" < ../lics/CurveBotDef.kyx >> tmp.kya
+sed "s/ArchiveEntry *\".*\"/Exercise \"POPL\/POPL: 4 Exercise: Bouncing ball\"/" < ../lics/bouncing-ball-exc.kyx >> tmp.kya
 cat > repls <<- EOM
-s/ArchiveEntry *".*"/ArchiveEntry "POPL: 5 Damped oscillator"/1
-s/ArchiveEntry *".*"/ArchiveEntry "POPL: 5 Increasingly damped oscillator"/2
+s/ArchiveEntry *".*"/ArchiveEntry "POPL\/POPL: 5 Damped oscillator"/1
+s/ArchiveEntry *".*"/ArchiveEntry "POPL\/POPL: 5 Increasingly damped oscillator"/2
 EOM
 sed -f repls < ../lics/damposc.kyx >> tmp.kya
-sed "s/Exercise *\".*\"/Exercise \"POPL: 6 Exercise: Parachute\"/" < ../dlds/parachuteExc.kyx >> tmp.kya
-sed "s/ArchiveEntry *\".*\"/ArchiveEntry \"POPL: 6 Solution: Parachute\"/" < ../dlds/parachute.kyx >> tmp.kya
+sed "s/Exercise *\".*\"/Exercise \"POPL\/POPL: 6 Exercise: Parachute\"/" < ../dlds/parachuteExc.kyx >> tmp.kya
+sed "s/ArchiveEntry *\".*\"/ArchiveEntry \"POPL\/POPL: 6 Solution: Parachute\"/" < ../dlds/parachute.kyx >> tmp.kya
 
 #cat ../lics/bouncing-ball.kya  ../lics/lics2-hybrid-forward.kya ../lics/lics4a-time-safe.kya ../lics/CurveBotDef.kya ../lics/rotational.kya ../lics/damposc.kyx ../dlds/parachute.kya > tmp.kya 
 perl -pe 's/\xEF\xBB\xBF//g' < tmp.kya > popltutorial.kyx
