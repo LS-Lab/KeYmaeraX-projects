199d4e991bb11c408f7d79af7971fbf0ceda7491
Stefan Mitsch <smitsch@andrew.cmu.edu>
2022-07-29 18:28:29 -0400

    Tutorial syntax update

--- games/make.sh
+++ games/make.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-cat dual-filibuster.kya pusharound-cart.kya WALL-E-EVE.kya goalie.kya > games.kya 
+cat dual-filibuster.kya pusharound-cart.kya WALL-E-EVE.kyx goalie.kya > games.kya
 perl -pe 's/\xEF\xBB\xBF//g' -i games.kya 
